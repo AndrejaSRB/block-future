5a9984c9aac044179b0c723d9fa10ea557e8ba43
AndrejaSRB <andreja.kojadinovic@gmail.com>
2024-10-16 11:53:28 +0200

    feat(configuration): add eslint, prettier, unimported and pre-commit

---
+++ .eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  root: true,
+  extends: [
+    'next',
+    'next/typescript',
+    'eslint:recommended',
+    'prettier',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    // JavaScript rules
+    'prefer-const': 'warn',
+    'no-var': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'object-shorthand': 'warn',
+    'quote-props': ['warn', 'as-needed'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    // TypeScript rules
+    '@typescript-eslint/array-type': [
+      'warn',
+      {
+        default: 'array',
+      },
+    ],
+    '@typescript-eslint/consistent-type-assertions': [
+      'warn',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'never',
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    // React rules
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['ts', 'tsx'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
